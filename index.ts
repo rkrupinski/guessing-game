fe895fd738a363063f43673832a4810b167231f7
Rafał Krupiński <krupinski.rafal@gmail.com>
2022-07-16 20:11:52 +0200

    feat: fixed winning number, one chance

--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
-const greeting: string = "Hello";
+import readline from "readline";
 
-console.log(greeting);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const MIN = 1;
+const MAX = 10;
+const WINNING_NUMBER = 8;
+
+rl.question(`Guess a number between ${MIN} and ${MAX}:\n`, (res) => {
+  const num = parseInt(res, 10);
+
+  rl.write(`You ${num === WINNING_NUMBER ? "win :)" : "lose :("}\n`);
+  rl.close();
+});
