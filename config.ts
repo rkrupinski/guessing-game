6642faddbd1069e92866361041a0f47e9ef2d54b
Rafał Krupiński <krupinski.rafal@gmail.com>
2022-07-17 14:50:23 +0200

    feat: N dumb moves ends the game

--- config.ts
+++ config.ts
@@ -1,3 +1,4 @@
 export const MIN = 1;
 export const MAX = 100;
 export const MAX_ATTEMPTS = 5;
+export const MAX_DUMB_ATTEMPTS = 3;
--- game.ts
+++ game.ts
@@ -1,8 +1,11 @@
 import { draw } from './utils';
-import { MIN, MAX, MAX_ATTEMPTS } from './config';
+import { MIN, MAX, MAX_ATTEMPTS, MAX_DUMB_ATTEMPTS } from './config';
 
 const $$winningNumber: unique symbol = Symbol('$$winningNumber');
 type $$winningNumber = typeof $$winningNumber;
+
+const $$dumbAttempts: unique symbol = Symbol('$$dumbAttempts');
+type $$dumbAttempts = typeof $$dumbAttempts;
 
 const $$attempts: unique symbol = Symbol('$$attempts');
 type $$attempts = typeof $$attempts;
@@ -12,6 +15,7 @@ type $$prev = typeof $$prev;
 
 type InternalGameState = {
   [$$winningNumber]: number;
+  [$$dumbAttempts]: number;
   [$$attempts]: number;
   [$$prev]: number[];
 };
@@ -39,6 +43,7 @@ export const makeInitialState = (): GameState => ({
   type: 'playing',
   msg: `Guess a number between ${MIN} and ${MAX}:`,
   [$$winningNumber]: draw(MIN, MAX),
+  [$$dumbAttempts]: 0,
   [$$attempts]: 0,
   [$$prev]: [],
 });
@@ -55,20 +60,29 @@ export const game = (state: GameState, action: GameAction): GameState => {
           msg: 'You win',
         };
 
+      const prev = state[$$prev].length
+        ? state[$$prev][state[$$prev].length - 1]
+        : null;
+
+      const repeated = state[$$prev].includes(action.value);
+
+      const hintIgnored =
+        typeof prev === 'number' &&
+        ((action.value < prev && action.value < state[$$winningNumber]) ||
+          (action.value > prev && action.value > state[$$winningNumber]));
+
+      const isDumbAttempt = repeated || hintIgnored;
+
+      const dumpAttempts = state[$$dumbAttempts] + 1;
+
+      if (dumpAttempts === MAX_DUMB_ATTEMPTS)
+        return {
+          ...state,
+          type: 'game_over',
+          msg: "This isn't working",
+        };
+
       if (attempts < MAX_ATTEMPTS) {
-        const prev = state[$$prev].length
-          ? state[$$prev][state[$$prev].length - 1]
-          : null;
-
-        const repeated = state[$$prev].includes(action.value);
-
-        const hintIgnored =
-          typeof prev === 'number' &&
-          ((action.value < prev && action.value < state[$$winningNumber]) ||
-            (action.value > prev && action.value > state[$$winningNumber]));
-
-        const isDumbAttempt = repeated || hintIgnored;
-
         const hint =
           action.value > state[$$winningNumber] ? 'smaller' : 'greater';
 
@@ -82,6 +96,9 @@ export const game = (state: GameState, action: GameAction): GameState => {
           msg,
           [$$attempts]: attempts,
           [$$prev]: [...state[$$prev], action.value],
+          [$$dumbAttempts]: isDumbAttempt
+            ? state[$$dumbAttempts] + 1
+            : state[$$dumbAttempts],
         };
       }
 
@@ -91,6 +108,7 @@ export const game = (state: GameState, action: GameAction): GameState => {
         msg: 'You lose',
       };
     }
+
     default:
       return state;
   }
